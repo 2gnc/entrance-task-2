b961eae203d888711186dba28dbdffd5941fd9dd
Ksenia Polyakova <ksenia.v.polyakova@mail.ru>
2017-12-21 12:21:51 +0300

    добавила минификацию css

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 
 let gulp = require( 'gulp' ),
 	sass = require( 'gulp-sass' ),
+	csso = require( 'gulp-csso' ),
 	rename = require( 'gulp-rename' ),
 	del = require( 'del' );
 
@@ -9,7 +10,8 @@ gulp.task( 'sass', function() {
 	return gulp.src( 'src/styles/main.sass' )
 		.pipe( sass().on('error', sass.logError) )
 		.pipe( gulp.dest( 'temp' ) )
-		.pipe( rename( 'style.css' ) )
+		.pipe( csso() )
+		.pipe( rename( 'style.min.css' ) )
 		.pipe( gulp.dest( 'public/styles/' ) );
 } );
 
