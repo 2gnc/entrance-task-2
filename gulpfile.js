6603cd3f034f313ac3d17e623b88c1dd413d1897
Ksenia Polyakova <ksenia.v.polyakova@mail.ru>
2017-12-21 13:16:02 +0300

    добавила сборку html

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,23 @@
 let gulp = require( 'gulp' ),
 	sass = require( 'gulp-sass' ),
 	csso = require( 'gulp-csso' ),
+	pug = require( 'gulp-pug' ),
 	rename = require( 'gulp-rename' ),
 	del = require( 'del' );
 
-gulp.task( 'sass', function() {
+gulp.task( 'css', function() {
 	return gulp.src( 'src/styles/main.sass' )
 		.pipe( sass().on('error', sass.logError) )
 		.pipe( gulp.dest( 'temp' ) )
 		.pipe( csso() )
 		.pipe( rename( 'style.min.css' ) )
 		.pipe( gulp.dest( 'public/styles/' ) );
+} );
+
+gulp.task( 'html', function() {
+	return  gulp.src( 'src/pages/**.pug' )
+		.pipe( pug() )
+		.pipe( gulp.dest( 'public/' ) );
 } );
 
 gulp.task( 'cleanup', function() {
