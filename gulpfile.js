5922180b0c9a4f0e56aaa37286c859049f2e0346
Ksenia Polyakova <ksenia.v.polyakova@mail.ru>
2017-12-21 12:00:27 +0300

    добавлены таски sass, cleanup и заглушки для default и production

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,22 @@
-let gulp = require( 'gulp' );
+'use strict';
 
+let gulp = require( 'gulp' ),
+	sass = require( 'gulp-sass' ),
+	rename = require( 'gulp-rename' ),
+	del = require( 'del' );
 
-gulp.task('default', console.log('it works'));
+gulp.task( 'sass', function() {
+	return gulp.src( 'src/styles/main.sass' )
+		.pipe( sass().on('error', sass.logError) )
+		.pipe( gulp.dest( 'temp' ) )
+		.pipe( rename( 'style.css' ) )
+		.pipe( gulp.dest( 'public/styles/' ) );
+} );
+
+gulp.task( 'cleanup', function() {
+	return del( ['temp/**.*'] );
+} );
+
+gulp.task( 'default', console.log( 'it works' ) );
+
+gulp.task( 'production', console.log( 'тут будет большой таск для продакшн' ) );
