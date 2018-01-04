ec3a0ba76c8e2aad2b8adbcddb4066dc89a5c465
Polyakova Kseniya <ksenia.v.polyakova@mail.ru>
2018-01-04 21:53:29 +0300

    подключила блок autocomplete

---
+++ src/blocks/autocomplete/autocomplete.js
@@ -0,0 +1,10 @@
+$(function() {
+	$( '.autocomplete' ).niceScroll(
+		"div.autocomplete__wrapper",
+		{
+			cursorwidth: "8px",
+			cursorcolor:"#D5DFE9",
+			cursorborderradius: "100px"
+		}
+	);
+});
