1647b334dbe55756dbfe2b1f09f7c83ff976efe5
Polyakova Kseniya <ksenia.v.polyakova@mail.ru>
2018-01-08 18:22:01 +0300

    состояние для имени комнаты при скролле

--- src/blocks/room/room.js
+++ src/blocks/room/room.js
@@ -1,12 +1,22 @@
-//с троттлером при скролле проверить, не скрылся ли блок schedule__aside полностью с экрана
-// если скрылся полностью, то добавиль модификатор --scrolled к .room__name
-
-	//const aside = $('.schedule__aside')[0];
-	const schedule = document.getElementsByClassName('schedule')[0];
-
-	console.log(schedule);
-
-	document.onscroll = function() {
-		console.log( 'foo ' + window.pageXOffset )
-	};
+(function (){
+	const aside = $ ( '.schedule__aside' )[ 0 ].clientWidth;
+	let check = false;
+	const rooms = $ ( '.room__name' );
+	
+	document.addEventListener ( 'scroll', switcher );
+	
+	function switcher () {
+		
+		if ( window.pageXOffset > aside && !check ) {
+			check = true;
+			rooms.toggleClass ( 'room__name--swiped' );
+		}
+		
+		if ( window.pageXOffset <= aside && check ) {
+			check = false;
+			rooms.toggleClass ( 'room__name--swiped' );
+		}
+		
+	}
+}());
 	
