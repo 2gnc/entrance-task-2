fd39248c6ff7dcec590b08076ab358c3f5941eef
Polyakova Kseniya <ksenia.v.polyakova@mail.ru>
2018-01-08 16:47:06 +0300

    убрала overflow: из-за него не срабатывало событие горизонтального скролла

--- src/blocks/room/room.js
+++ src/blocks/room/room.js
@@ -1,1 +1,12 @@
-console.log("test");
+//с троттлером при скролле проверить, не скрылся ли блок schedule__aside полностью с экрана
+// если скрылся полностью, то добавиль модификатор --scrolled к .room__name
+
+	//const aside = $('.schedule__aside')[0];
+	const schedule = document.getElementsByClassName('schedule')[0];
+
+	console.log(schedule);
+
+	document.onscroll = function() {
+		console.log( 'foo ' + window.pageXOffset )
+	};
+	
