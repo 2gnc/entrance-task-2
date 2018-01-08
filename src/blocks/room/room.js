8fdc25e34c553027146be62962b5bd2493acef31
Polyakova Kseniya <ksenia.v.polyakova@mail.ru>
2018-01-08 19:22:24 +0300

    сделала отображение названия комнаты и этажа при скролле

--- src/blocks/room/room.js
+++ src/blocks/room/room.js
@@ -1,20 +1,33 @@
 (function (){
-	const aside = $ ( '.schedule__aside' )[ 0 ].clientWidth;
-	let check = false;
+	const aside = $( '.schedule__aside' )[ 0 ].clientWidth;
+	const roomsbox = $( '.rooms' );
 	const rooms = $ ( '.room__name' );
+	const floors = $( '.rooms__floor-name' );
+	let check = false;
 	
 	document.addEventListener ( 'scroll', switcher );
 	
 	function switcher () {
 		
+		console.log( 'ff' );
+		
 		if ( window.pageXOffset > aside && !check ) {
 			check = true;
 			rooms.toggleClass ( 'room__name--swiped' );
+			roomsbox.toggleClass( 'rooms--swiped' );
+			floors.toggleClass( 'rooms__floor-name--swiped' );
 		}
 		
 		if ( window.pageXOffset <= aside && check ) {
 			check = false;
 			rooms.toggleClass ( 'room__name--swiped' );
+			roomsbox.toggleClass( 'rooms--swiped' );
+			floors.toggleClass( 'rooms__floor-name--swiped' );
+			roomsbox.removeAttr( 'style' );
+		}
+		
+		if( window.pageXOffset > aside ) {
+			roomsbox.css( 'left', pageXOffset );
 		}
 		
 	}
