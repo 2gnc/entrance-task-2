4699c9d6f003c636588908257e4feaaa5ca1ee19
Ksenia Polyakova <ksenia.v.polyakova@mail.ru>
2018-01-09 14:36:24 +0300

    добавила проверку на наличие aside

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -18,35 +18,37 @@ $(function() {
 	);
 });
 (function (){
-	const aside = $( '.schedule__aside' )[ 0 ].clientWidth;
-	const roomsbox = $( '.rooms' );
-	const rooms = $ ( '.room__name' );
-	const floors = $( '.rooms__floor-name' );
-	let check = false;
-	
-	document.addEventListener ( 'scroll', switcher );
-	
-	function switcher () {
+	if ( $( '.schedule__aside' ).length > 0 ) {
+		const aside = $( '.schedule__aside' )[ 0 ].clientWidth;
+		const roomsbox = $( '.rooms' );
+		const rooms = $ ( '.room__name' );
+		const floors = $( '.rooms__floor-name' );
+		let check = false;
 		
-		if ( window.pageXOffset > aside && !check ) {
-			check = true;
-			rooms.toggleClass ( 'room__name--swiped' );
-			roomsbox.toggleClass( 'rooms--swiped' );
-			floors.toggleClass( 'rooms__floor-name--swiped' );
+		document.addEventListener ( 'scroll', switcher );
+		
+		function switcher () {
+			
+			if ( window.pageXOffset > aside && !check ) {
+				check = true;
+				rooms.toggleClass ( 'room__name--swiped' );
+				roomsbox.toggleClass( 'rooms--swiped' );
+				floors.toggleClass( 'rooms__floor-name--swiped' );
+			}
+			
+			if ( window.pageXOffset <= aside && check ) {
+				check = false;
+				rooms.toggleClass ( 'room__name--swiped' );
+				roomsbox.toggleClass( 'rooms--swiped' );
+				floors.toggleClass( 'rooms__floor-name--swiped' );
+				roomsbox.removeAttr( 'style' );
+			}
+			
+			if( window.pageXOffset > aside ) {
+				roomsbox.css( 'left', pageXOffset );
+			}
+			
 		}
-		
-		if ( window.pageXOffset <= aside && check ) {
-			check = false;
-			rooms.toggleClass ( 'room__name--swiped' );
-			roomsbox.toggleClass( 'rooms--swiped' );
-			floors.toggleClass( 'rooms__floor-name--swiped' );
-			roomsbox.removeAttr( 'style' );
-		}
-		
-		if( window.pageXOffset > aside ) {
-			roomsbox.css( 'left', pageXOffset );
-		}
-		
 	}
 }());
 	
